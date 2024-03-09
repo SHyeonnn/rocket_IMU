ccce496fb79cfdbe7b707d1bab260d5fed896bef
Chung-MinJun <myhome722@kyonggi.ac.kr>
2024-03-09 20:19:57 +0900

    시작입니다 이제

---
+++ main.ino
@@ -0,0 +1,21 @@
+/* 센서관련된 클래스가 정의된 cpp, 헤더파일은 Src 폴더로 넣어주시기 바랍니다*/
+
+/* #include begin--------------------------- */
+#include "Src/MPU9250.h" //#include "폴더명/헤더파일명.h"로 하시면 됩니다.
+/* #include end--------------------------- */
+
+/* #define begin--------------------------- */
+#define PI = 3.1415926535897932384626433832795028
+/* #define end--------------------------- */
+
+/* Global Variables initial begin--------------*/
+
+/* Global Variables initial end--------------*/
+
+void setup()
+{
+}
+
+void loop()
+{
+}
