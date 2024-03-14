5d039ab3282bb46124d5b6aec566f3c482b79efe
Chung-MinJun <myhome722@kyonggi.ac.kr>
2024-03-14 23:06:45 +0900

    Update rocket_IMU.ino

--- rocket_IMU.ino
+++ rocket_IMU.ino
@@ -2,6 +2,7 @@
 
 /* #include begin--------------------------- */
 #include "Src/MPU9250.h" //#include "폴더명/헤더파일명.h"로 하시면 됩니다.
+#include <SoftwareSerial.h>
 /* #include end--------------------------- */
 
 /* #define begin--------------------------- */
@@ -9,13 +10,23 @@
 /* #define end--------------------------- */
 
 /* Global Variables initial begin--------------*/
-
+SoftwareSerial Gps(2,3); //(RxPin, TxPin)
+//softwareSerial port speed up to 115200 bps
+String tempGPS;
 /* Global Variables initial end--------------*/
 
 void setup()
 {
+  Serial.begin(9600);
+  if(Gps.available()){
+    Serial.println("GPS is Ready...")
+    delay(1500);
+  }
 }
 
 void loop()
 {
+  if(Gps.available()){
+    tempGPS = Gps.read();
+  }
 }
